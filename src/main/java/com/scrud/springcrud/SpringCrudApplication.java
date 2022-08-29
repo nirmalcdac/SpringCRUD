fdd98fef9c44708f3f01d723ba01a77ee11fcccc
nirmalsharma <nirmal.sharma3500@gmail.com>
2022-08-30 00:20:47 +0530

    removed commented code

--- src/main/java/com/scrud/springcrud/SpringCrudApplication.java
+++ src/main/java/com/scrud/springcrud/SpringCrudApplication.java
@@ -1,7 +1,5 @@
 package com.scrud.springcrud;
 
-import com.scrud.springcrud.repository.EmployeeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,24 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringCrudApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCrudApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCrudApplication.class, args);
+    }
 
-	@Autowired
-	private EmployeeRepository employeeRepository;
-	@Override
-	public void run(String... args) throws Exception {
-//		Employee employee = new Employee();
-//		employee.setFirstName("Nalini");
-//		employee.setLastName("Nalini");
-//		employee.setEmailId("drnalininirmal@gmail.com");
-//		employeeRepository.save(employee);
-//
-//		Employee employee1 = new Employee();
-//		employee1.setFirstName("Nalini1");
-//		employee1.setLastName("Nalini1");
-//		employee1.setEmailId("drnalininirmal1@gmail.com");
-//		employeeRepository.save(employee1);
-	}
+    @Override
+    public void run(String... args) throws Exception {
+    }
 }
